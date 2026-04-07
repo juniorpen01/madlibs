3d241c871e5ad5b81ac23ac40ae6e8a23ea97418
juniorpen01 <01juniorpen@gmail.com>
2026-04-08 02:32:03 +0800

    Implement up to picking template

--- cmd/repl/repl.go
+++ cmd/repl/repl.go
@@ -3,26 +3,44 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
+	"math/rand"
 	"os"
+	"path/filepath"
 	"strings"
 )
+
+var scanner = bufio.NewScanner(os.Stdin)
 
 func main() {
 	fmt.Println("hello world")
 
-	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(getWantsToPlay())
 
-	switch sanitizeInput(getInput(scanner, "Do you want to play the game? (Y/n): ")) {
-	case "", "Y", "YES":
-		panic("todo")
-	case "N", "NO":
-		panic("todo")
-	default:
-		panic("todo")
+	file, err := os.Open(pickTemplate())
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for i := 0; scanner.Scan(); i += 1 {
+		fmt.Println(i, scanner.Text())
 	}
 }
 
-func getInput(scanner *bufio.Scanner, prompt string) string {
+func getWantsToPlay() bool {
+	switch sanitizeInput(getInput("Do you want to play the game? (Y/n): ")) {
+	case "", "Y", "YES":
+		return true
+	case "N", "NO":
+		return false
+	default:
+		fmt.Println("Invalid input")
+		return getWantsToPlay()
+	}
+}
+
+func getInput(prompt string) string {
 	fmt.Print(prompt)
 	scanner.Scan()
 	// NOTE: scanner.Err?
@@ -30,6 +48,36 @@ func getInput(scanner *bufio.Scanner, prompt string) string {
 }
 func sanitizeInput(input string) string {
 	return strings.ToUpper(input)
+}
+
+// im gonna assume not buggy at all
+func pickTemplate() string {
+	i := 0
+
+	curPath := ""
+
+	filepath.WalkDir("./res", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		i += 1
+
+		if rand.Intn(i) == 0 {
+			curPath = path
+		}
+
+		return nil
+	})
+
+	return curPath
+
+	// file, _ := os.Open("./res/choose_a_file.txt")
+	// return file
 }
 
 // regex in: <\s*(.)(.+?)\s*> out:{{ .\u$1$2 }} DAMNNN
